03e940b894091b191944aeaeb2331e2e3c0194c1
James Xu <xujames007@gmail.com>
2019-01-25 09:27:54 -0500

    beginning of projectile spawner

--- core/src/com/mygdx/game/Spawners/ProjectileSpawner.java
+++ core/src/com/mygdx/game/Spawners/ProjectileSpawner.java
@@ -1,4 +1,16 @@
 package com.mygdx.game.Spawners;
 
-public class ProjectileSpawner {
+public class ProjectileSpawner extends Spawner {
+    public ProjectileSpawner() {
+    }
+
+    @Override
+    public void run(float delta) {
+        super.run(delta);
+    }
+
+    @Override
+    public void spawn(int x, int y, int index) {
+
+    }
 }
