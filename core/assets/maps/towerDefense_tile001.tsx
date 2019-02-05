9759192058c1b3d4469f1608f71ab4f40e275a69
Nithin Muthukumar <kathrikai30201094@gmail.com>
2019-02-05 00:56:33 -0500

    getting stats from json files

--- core/assets/maps/towerDefense_tile001.tsx
+++ core/assets/maps/towerDefense_tile001.tsx
@@ -1,4 +1,4 @@
-<;?xml version="1.0" encoding="UTF-8"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.1" name="towerDefense_tile001" tilewidth="64" tileheight="64" tilecount="299"
          columns="0">
     <grid orientation="orthogonal" width="1" height="1"/>
