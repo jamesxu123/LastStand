5435a1e9a6cd27ac18fc183382d96858f5257360
James Xu <xujames007@gmail.com>
2019-02-10 15:36:06 -0500

    new backend

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,7 +1,8 @@
 package com.mygdx.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
 import com.mygdx.game.LastStand;
 
 import static com.mygdx.game.LastStand.screenH;
@@ -9,10 +10,11 @@ import static com.mygdx.game.LastStand.screenW;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = screenW;
-		config.height = screenH;
-		config.resizable = false;
-		new LwjglApplication(new LastStand(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(screenW, screenH);
+		config.setHdpiMode(Lwjgl3ApplicationConfiguration.HdpiMode.Logical);
+		config.setResizable(false);
+		config.setIdleFPS(30);
+		new Lwjgl3Application(new LastStand(), config);
 	}
 }
